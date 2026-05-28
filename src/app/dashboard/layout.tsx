deca0eaa02f1a783f93154d24bc66b70010a28c5
Solit Computer <solitcomputer.03@gmail.com>
2026-05-28 16:52:34 +0700

    update

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
   React.ReactNode;
 }) {
   return (
-    <main className="p-4 lg:p-6 space-y-6">
+    <main className="p-3 lg:p-6 space-y-6 ">
       {children}
     </main>
   );
