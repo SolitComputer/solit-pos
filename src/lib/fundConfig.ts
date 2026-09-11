c583bf19399d7530f214e25cead4ca31863f34c9
Ojan Abdul <ojanabdul83@gmail.com>
2026-09-11 20:24:51 +0700

    merge moreno

---
+++ src/lib/fundConfig.ts
@@ -0,0 +1,4 @@
+import { YOGA_ADMIN_ID, REINALDY_ADMIN_ID } from "@/lib/contractSigners";
+
+export const FUND_EXECUTOR_IDS: string[] = ["157585d5-af25-4271-80c6-93160b3f8975"];
+export const FUND_APPROVER_IDS: string[] = [YOGA_ADMIN_ID, REINALDY_ADMIN_ID];
